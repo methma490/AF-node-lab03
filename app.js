6458947bf63c56317503b4cf5dd709e874fed3b6
methma490 <methmagk@gmail.com>
2026-02-22 15:05:01 +0530

    Promise example

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello Node Lab 03");
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) resolve("Success!");
+  else reject("Failure!");
+});
+
+myPromise
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
