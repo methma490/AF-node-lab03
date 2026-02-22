773b0c67444dae7def2a88fc8a83e9544ed6c088
methma490 <methmagk@gmail.com>
2026-02-22 15:03:00 +0530

    HTTP request example

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello Node Lab 03");
+const https = require("https");
+
+https
+  .get("https://jsonplaceholder.typicode.com/posts/1", (resp) => {
+    let data = "";
+
+    resp.on("data", (chunk) => (data += chunk));
+    resp.on("end", () => console.log(JSON.parse(data)));
+  })
+  .on("error", (err) => console.log("Error: " + err.message));
